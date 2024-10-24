8cd193b82cf06c61ba094a5eae88494b6974613a
suguruwataru <23465321+ii41@users.noreply.github.com>
2024-10-25 01:41:01 +0800

    Move build directories into one to make root directory cleaner.

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
-    println!("cargo:rustc-link-search=meson_build");
+    println!("cargo:rustc-link-search=build/meson");
     println!("cargo:rustc-link-lib=rimed");
     println!("cargo:rustc-link-lib=rime");
-    println!("cargo:rerun-if-changed=meson_build/librimed.a");
+    println!("cargo:rerun-if-changed=build/meson/librimed.a");
 }
