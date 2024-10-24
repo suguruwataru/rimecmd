b4db87dc3c521ced11a11d22a00e35e32d8b6ddb
suguruwataru <23465321+ii41@users.noreply.github.com>
2024-10-25 01:41:02 +0800

    Rename rimed to rimecmd in C source

--- src/rime_api/rime_api.c
+++ src/rime_api/rime_api.c
@@ -13,7 +13,7 @@ void c_setup_rime_api_once(RimeApi *rime_api, const char *user_data_dir,
                            const char *shared_data_dir, int log_level) {
   RIME_STRUCT(RimeTraits, rime_traits);
   rime_traits.min_log_level = log_level;
-  rime_traits.app_name = "rime.rimed";
+  rime_traits.app_name = "rime.rimecmd";
   rime_traits.user_data_dir = user_data_dir;
   rime_traits.shared_data_dir = shared_data_dir;
   rime_api->setup(&rime_traits);
@@ -23,7 +23,7 @@ void c_initialize_rime_api(RimeApi *rime_api, const char *user_data_dir,
                            const char *shared_data_dir, int log_level) {
   RIME_STRUCT(RimeTraits, rime_traits);
   rime_traits.min_log_level = log_level;
-  rime_traits.app_name = "rime.rimed";
+  rime_traits.app_name = "rime.rimecmd";
   rime_traits.user_data_dir = user_data_dir;
   rime_traits.shared_data_dir = shared_data_dir;
   rime_api->initialize(&rime_traits);
@@ -64,7 +64,7 @@ void c_destory_session(RimeApi *rime_api, RimeSessionId session_id) {
   rime_api->destroy_session(session_id);
 }
 
-typedef struct rimed_rime_status {
+typedef struct rimecmd_rime_status {
   char *schema_name;
   char *schema_id;
   Bool is_disabled;
@@ -74,44 +74,44 @@ typedef struct rimed_rime_status {
   Bool is_simplified;
   Bool is_traditional;
   Bool is_ascii_punct;
-} RimedRimeStatus;
+} RimecmdRimeStatus;
 
 void c_get_status(RimeApi *rime_api, RimeSessionId session_id,
-                  RimedRimeStatus *rimed_status) {
+                  RimecmdRimeStatus *rimecmd_status) {
   RIME_STRUCT(RimeStatus, rime_status);
   rime_api->get_status(session_id, &rime_status);
-  rimed_status->schema_name = strdup(rime_status.schema_name);
-  rimed_status->schema_id = strdup(rime_status.schema_id);
-  rimed_status->is_disabled = rime_status.is_disabled;
-  rimed_status->is_composing = rime_status.is_composing;
-  rimed_status->is_ascii_mode = rime_status.is_ascii_mode;
-  rimed_status->is_full_shape = rime_status.is_full_shape;
-  rimed_status->is_simplified = rime_status.is_simplified;
-  rimed_status->is_traditional = rime_status.is_traditional;
-  rimed_status->is_ascii_punct = rime_status.is_ascii_punct;
+  rimecmd_status->schema_name = strdup(rime_status.schema_name);
+  rimecmd_status->schema_id = strdup(rime_status.schema_id);
+  rimecmd_status->is_disabled = rime_status.is_disabled;
+  rimecmd_status->is_composing = rime_status.is_composing;
+  rimecmd_status->is_ascii_mode = rime_status.is_ascii_mode;
+  rimecmd_status->is_full_shape = rime_status.is_full_shape;
+  rimecmd_status->is_simplified = rime_status.is_simplified;
+  rimecmd_status->is_traditional = rime_status.is_traditional;
+  rimecmd_status->is_ascii_punct = rime_status.is_ascii_punct;
   rime_api->free_status(&rime_status);
 }
 
-void c_free_status(RimedRimeStatus *status) {
+void c_free_status(RimecmdRimeStatus *status) {
   free(status->schema_name);
   free(status->schema_id);
 }
 
-typedef struct rimed_rime_commit {
+typedef struct rimecmd_rime_commit {
   char *text;
-} RimedRimeCommit;
+} RimecmdRimeCommit;
 
 void c_get_commit(RimeApi *rime_api, RimeSessionId session_id,
-                  RimedRimeCommit *rimed_commit) {
+                  RimecmdRimeCommit *rimecmd_commit) {
   RIME_STRUCT(RimeCommit, rime_commit);
   rime_api->get_commit(session_id, &rime_commit);
   if (rime_commit.text) {
-    rimed_commit->text = strdup(rime_commit.text);
+    rimecmd_commit->text = strdup(rime_commit.text);
   }
   rime_api->free_commit(&rime_commit);
 }
 
-void c_free_commit(RimedRimeCommit *commit) {
+void c_free_commit(RimecmdRimeCommit *commit) {
   if (commit->text)
     free(commit->text);
 }
@@ -121,29 +121,29 @@ Bool c_process_key(RimeApi *rime_api, RimeSessionId session_id, int keycode,
   return rime_api->process_key(session_id, keycode, mask);
 }
 
-typedef struct rimed_rime_context {
+typedef struct rimecmd_rime_context {
   RimeComposition composition;
   RimeMenu menu;
   char *commit_text_preview;
-} RimedRimeContext;
+} RimecmdRimeContext;
 
 void c_get_context(RimeApi *rime_api, RimeSessionId session_id,
-                   RimedRimeContext *rimed_context) {
+                   RimecmdRimeContext *rimecmd_context) {
   RIME_STRUCT(RimeContext, rime_context);
   rime_api->get_context(session_id, &rime_context);
   if (rime_context.commit_text_preview) {
-    rimed_context->commit_text_preview =
+    rimecmd_context->commit_text_preview =
         strdup(rime_context.commit_text_preview);
   }
-  rimed_context->composition = rime_context.composition;
+  rimecmd_context->composition = rime_context.composition;
   if (rime_context.composition.preedit)
-    rimed_context->composition.preedit =
+    rimecmd_context->composition.preedit =
         strdup(rime_context.composition.preedit);
-  rimed_context->menu = rime_context.menu;
+  rimecmd_context->menu = rime_context.menu;
   rime_api->free_context(&rime_context);
 }
 
-void c_free_context(RimedRimeContext *context) {
+void c_free_context(RimecmdRimeContext *context) {
   if (context->commit_text_preview)
     free(context->commit_text_preview);
   if (context->composition.preedit)
