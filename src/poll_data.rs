2f1e62c49a85f59f7a28f82ef29d3c9384b06b9d
suguruwataru <23465321+ii41@users.noreply.github.com>
2024-10-25 01:41:04 +0800

    Remove dependency on mio
    epoll, which by default is level-triggered, and can work
    with all the other operations, is much better. The only
    thing is that it's not portable, which isn't a concern
    for now.

--- src/poll_data.rs
+++ src/poll_data.rs
@@ -1,8 +1,7 @@
 use crate::{Error, Result};
-use mio::{unix::SourceFd, Events, Interest, Poll, Token};
 use std::cell::RefCell;
 use std::collections::HashMap;
-use std::os::fd::AsRawFd;
+use std::os::fd::{AsRawFd, FromRawFd, OwnedFd};
 use std::rc::Rc;
 
 pub trait ReadData<D> {
@@ -11,23 +10,23 @@ pub trait ReadData<D> {
 }
 
 pub struct PollData<D> {
-    poll: Poll,
-    counter: usize,
-    token_source_map: HashMap<usize, Rc<RefCell<dyn ReadData<D>>>>,
+    epoll: OwnedFd,
+    counter: u64,
+    id_source_map: HashMap<u64, Rc<RefCell<dyn ReadData<D>>>>,
     result_buffer: std::collections::VecDeque<D>,
 }
 
 impl<D> PollData<D> {
     pub fn new(sources: &[Rc<RefCell<dyn ReadData<D>>>]) -> Result<Self> {
         let mut poll_request = Self {
-            poll: Poll::new()?,
+            epoll: unsafe { OwnedFd::from_raw_fd(libc::epoll_create1(0)) },
             counter: 0,
             result_buffer: vec![].into(),
-            token_source_map: HashMap::new(),
+            id_source_map: HashMap::new(),
         };
         for source in sources.into_iter() {
             poll_request
-                .token_source_map
+                .id_source_map
                 .insert(poll_request.counter, Rc::clone(source));
             source.borrow().register(&mut poll_request)?;
         }
@@ -35,11 +34,21 @@ impl<D> PollData<D> {
     }
 
     pub fn register(&mut self, source: &impl AsRawFd) -> Result<()> {
-        self.poll.registry().register(
-            &mut SourceFd(&source.as_raw_fd()),
-            Token(self.counter),
-            Interest::READABLE,
-        )?;
+        if -1
+            == unsafe {
+                libc::epoll_ctl(
+                    self.epoll.as_raw_fd(),
+                    libc::EPOLL_CTL_ADD,
+                    source.as_raw_fd(),
+                    &mut libc::epoll_event {
+                        events: libc::EPOLLIN as u32,
+                        u64: self.counter,
+                    },
+                )
+            }
+        {
+            return Err(crate::Error::Io(std::io::Error::last_os_error())).unwrap();
+        };
         self.counter += 1;
         Ok(())
     }
@@ -49,15 +58,32 @@ impl<D> PollData<D> {
         if let Some(ret) = ret {
             return Ok(ret);
         }
-        let mut events = Events::with_capacity(self.counter);
-        self.poll.poll(&mut events, None)?;
+        let events = {
+            let mut events = Vec::<libc::epoll_event>::with_capacity((self.counter * 4) as usize);
+            let maxevents = events.capacity() as i32;
+            let wait_return_value = unsafe {
+                libc::epoll_wait(self.epoll.as_raw_fd(), events.as_mut_ptr(), maxevents, -1)
+            };
+            if -1 == wait_return_value {
+                return Err(crate::Error::Io(std::io::Error::last_os_error())).unwrap();
+            } else {
+                unsafe { events.set_len(wait_return_value as usize) }
+            }
+            events
+        };
         for event in events.into_iter() {
-            if event.is_read_closed() {
+            if event.events & libc::EPOLLHUP as u32 != 0 {
                 return Err(Error::InputClosed);
             }
-            assert!(event.is_readable());
-            let source = self.token_source_map.get(&event.token().0).unwrap();
-            let data = source.borrow_mut().read_data()?;
+            assert!(event.events & libc::EPOLLIN as u32 != 0);
+            let source = self
+                .id_source_map
+                .get(&{
+                    let u64 = event.u64;
+                    u64
+                })
+                .unwrap();
+            let data = source.borrow_mut().read_data().unwrap();
             match ret {
                 None => ret = Some(data),
                 Some(_) => self.result_buffer.push_front(data),
