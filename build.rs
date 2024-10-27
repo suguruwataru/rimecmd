0b8de84fa107770befa1a4d2a222b68959e45117
suguruwataru <23465321+ii41@users.noreply.github.com>
2024-10-27 19:19:21 +0800

    Let src/rime_api/rime_api.c be re-compiled after change
    `cc` does not automatically enable re-compile on change for the
    files it uses. This is now added manually to `build.rs`.

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ fn main() {
     cc::Build::new()
         .file("src/rime_api/rime_api.c")
         .compile("rimecmd");
+    println!("cargo:rerun-if-changed=src/rime_api/rime_api.c");
     println!("cargo:rustc-link-lib=rimecmd");
     println!("cargo:rustc-link-lib=rime");
 }
